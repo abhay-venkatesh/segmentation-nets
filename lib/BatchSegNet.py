b47ed9f566b72f322a0fbfd6403504a79659fced
abhay-venkatesh <abhay.venkatesh@gmail.com>
2017-12-07 13:04:24 -0600

    Split nets into regular and batch versions

--- lib/BatchSegNet.py
+++ lib/BatchSegNet.py
@@ -15,7 +15,7 @@ class SegNet:
   and https://arxiv.org/pdf/1505.07293.pdf
   and https://arxiv.org/pdf/1511.00561.pdf '''
 
-  def __init__(self):
+  def __init__(self,):
     # Build the network
     self.build()
 
@@ -39,37 +39,75 @@ class SegNet:
     return tf.nn.max_pool_with_argmax(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
   # Implementation idea from: https://github.com/tensorflow/tensorflow/issues/2169
+  def unpool(self, pool, ind, ksize=(1, 2, 2, 1), scope='unpool'):
+    """ Unpooling layer after max_pool_with_argmax.
+      Args:
+        pool: max pooled output tensor
+        ind: argmax indices (produced by tf.nn.max_pool_with_argmax)
+        ksize: ksize is the same as for the pool
+      Return:
+        unpooled: unpooling tensor """
+    with tf.variable_scope(scope):
+      pooled_shape = tf.shape(pool) 
+      flatten_ind = tf.reshape(ind, (pooled_shape[0], pooled_shape[1] * pooled_shape[2] * pooled_shape[3]))
+      # sparse indices to dense ones_like matrics
+      one_hot_ind = tf.one_hot(flatten_ind,  pooled_shape[1] * ksize[1] * pooled_shape[2] * ksize[2] * pooled_shape[3], on_value=1., off_value=0., axis=-1)
+      one_hot_ind = tf.reduce_sum(one_hot_ind, axis=1)
+      one_like_mask = tf.reshape(one_hot_ind, (pooled_shape[0], pooled_shape[1] * ksize[1], pooled_shape[2] * ksize[2], pooled_shape[3]))
+      # resize input array to the output size by nearest neighbor
+      img = tf.image.resize_nearest_neighbor(pool, [pooled_shape[1] * ksize[1], pooled_shape[2] * ksize[2]])
+      unpooled = tf.multiply(img, tf.cast(one_like_mask, img.dtype))
+      return unpooled
+
   def unravel_argmax(self, argmax, shape):
     output_list = []
     output_list.append(argmax // (shape[2] * shape[3]))
     output_list.append(argmax % (shape[2] * shape[3]) // shape[3])
     return tf.stack(output_list)
   
-  def unpool_layer2x2(self, x, raveled_argmax, out_shape):
-    ''' Implementation idea from: 
-        https://github.com/tensorflow/tensorflow/issues/2169 '''
+  def unpool_layer2x2_batch(self, x, argmax):
+    '''
+    Args:
+        x: 4D tensor of shape [batch_size x height x width x channels]
+        argmax: A Tensor of type Targmax. 4-D. The flattened indices of the max
+        values chosen for each output.
+    Return:
+        4D output tensor of shape [batch_size x 2*height x 2*width x channels]
+    '''
+    x_shape = tf.shape(x)
+    out_shape = [x_shape[0], x_shape[1]*2, x_shape[2]*2, x_shape[3]]
 
-    argmax = self.unravel_argmax(raveled_argmax, tf.to_int64(out_shape))
-    output = tf.zeros([out_shape[1], out_shape[2], out_shape[3]])
-    height = tf.shape(output)[0]
-    width = tf.shape(output)[1]
-    channels = tf.shape(output)[2]
+    batch_size = out_shape[0]
+    height = out_shape[1]
+    width = out_shape[2]
+    channels = out_shape[3]
+
+    argmax_shape = tf.to_int64([batch_size, height, width, channels])
+    argmax = self.unravel_argmax(argmax, argmax_shape)
+
     t1 = tf.to_int64(tf.range(channels))
-    t1 = tf.tile(t1, [((width + 1) // 2) * ((height + 1) // 2)])
+    t1 = tf.tile(t1, [batch_size*(width//2)*(height//2)])
     t1 = tf.reshape(t1, [-1, channels])
     t1 = tf.transpose(t1, perm=[1, 0])
-    t1 = tf.reshape(t1, [channels, (height + 1) // 2, (width + 1) // 2, 1])
-    t2 = tf.squeeze(argmax)
-    t2 = tf.stack((t2[0], t2[1]), axis=0)
-    t2 = tf.transpose(t2, perm=[3, 1, 2, 0])
-    t = tf.concat([t2, t1], 3)
-    indices = tf.reshape(t, [((height + 1) // 2) * ((width + 1) // 2) * channels, 3])
-    x1 = tf.squeeze(x)
-    x1 = tf.reshape(x1, [-1, channels])
-    x1 = tf.transpose(x1, perm=[1, 0])
+    t1 = tf.reshape(t1, [channels, batch_size, height//2, width//2, 1])
+    t1 = tf.transpose(t1, perm=[1, 0, 2, 3, 4])
+
+    t2 = tf.to_int64(tf.range(batch_size))
+    t2 = tf.tile(t2, [channels*(width//2)*(height//2)])
+    t2 = tf.reshape(t2, [-1, batch_size])
+    t2 = tf.transpose(t2, perm=[1, 0])
+    t2 = tf.reshape(t2, [batch_size, channels, height//2, width//2, 1])
+
+    t3 = tf.transpose(argmax, perm=[1, 4, 2, 3, 0])
+
+    t = tf.concat([t2, t3, t1], 4)
+    indices = tf.reshape(t, [(height//2)*(width//2)*channels*batch_size, 4])
+
+    x1 = tf.transpose(x, perm=[0, 3, 1, 2])
     values = tf.reshape(x1, [-1])
-    delta = tf.SparseTensor(indices, values, tf.to_int64(tf.shape(output)))
-    return tf.expand_dims(tf.sparse_tensor_to_dense(tf.sparse_reorder(delta)), 0)
+
+    delta = tf.SparseTensor(indices, values, tf.to_int64(out_shape))
+    return tf.sparse_tensor_to_dense(tf.sparse_reorder(delta))
         
   def conv_layer(self, x, W_shape, b_shape, name, padding='SAME'):
     W = self.weight_variable(W_shape)
@@ -86,8 +124,8 @@ class SegNet:
 
   def build(self):
     # Declare placeholders
-    self.x = tf.placeholder(tf.float32, shape=(1, None, None, 3))
-    self.y = tf.placeholder(tf.int64, shape=(1, None, None))
+    self.x = tf.placeholder(tf.float32, shape=(None, None, None, 3))
+    self.y = tf.placeholder(tf.int64, shape=(None, None, None))
     expected = tf.expand_dims(self.y, -1)
     self.rate = tf.placeholder(tf.float32, shape=[])
 
@@ -127,30 +165,30 @@ class SegNet:
     deconv_fc_6 = self.deconv_layer(fc_7, [7, 7, 512, 4096], 512, 'fc6_deconv')
 
     # First decoder
-    unpool_5 = self.unpool_layer2x2(deconv_fc_6, pool_5_argmax, tf.shape(conv_5_3))
+    unpool_5 = self.unpool(deconv_fc_6, pool_5_argmax)
     deconv_5_3 = self.deconv_layer(unpool_5, [3, 3, 512, 512], 512, 'deconv_5_3')
     deconv_5_2 = self.deconv_layer(deconv_5_3, [3, 3, 512, 512], 512, 'deconv_5_2')
     deconv_5_1 = self.deconv_layer(deconv_5_2, [3, 3, 512, 512], 512, 'deconv_5_1')
 
     # Second decoder
-    unpool_4 = self.unpool_layer2x2(deconv_5_1, pool_4_argmax, tf.shape(conv_4_3))
+    unpool_4 = self.unpool(deconv_5_1, pool_4_argmax)
     deconv_4_3 = self.deconv_layer(unpool_4, [3, 3, 512, 512], 512, 'deconv_4_3')
     deconv_4_2 = self.deconv_layer(deconv_4_3, [3, 3, 512, 512], 512, 'deconv_4_2')
     deconv_4_1 = self.deconv_layer(deconv_4_2, [3, 3, 256, 512], 256, 'deconv_4_1')
 
     # Third decoder
-    unpool_3 = self.unpool_layer2x2(deconv_4_1, pool_3_argmax, tf.shape(conv_3_3))
+    unpool_3 = self.unpool(deconv_4_1, pool_3_argmax)
     deconv_3_3 = self.deconv_layer(unpool_3, [3, 3, 256, 256], 256, 'deconv_3_3')
     deconv_3_2 = self.deconv_layer(deconv_3_3, [3, 3, 256, 256], 256, 'deconv_3_2')
     deconv_3_1 = self.deconv_layer(deconv_3_2, [3, 3, 128, 256], 128, 'deconv_3_1')
 
     # Fourth decoder
-    unpool_2 = self.unpool_layer2x2(deconv_3_1, pool_2_argmax, tf.shape(conv_2_2))
+    unpool_2 = self.unpool(deconv_3_1, pool_2_argmax)
     deconv_2_2 = self.deconv_layer(unpool_2, [3, 3, 128, 128], 128, 'deconv_2_2')
     deconv_2_1 = self.deconv_layer(deconv_2_2, [3, 3, 64, 128], 64, 'deconv_2_1')
 
     # Fifth decoder
-    unpool_1 = self.unpool_layer2x2(deconv_2_1, pool_1_argmax, tf.shape(conv_1_2))
+    unpool_1 = self.unpool(deconv_2_1, pool_1_argmax)
     deconv_1_2 = self.deconv_layer(unpool_1, [3, 3, 64, 64], 64, 'deconv_1_2')
     deconv_1_1 = self.deconv_layer(deconv_1_2, [3, 3, 32, 64], 32, 'deconv_1_1')
 
@@ -166,6 +204,8 @@ class SegNet:
 
     # Metrics
     predicted_image = tf.argmax(preds, axis=3)
+    self.accuracy = tf.contrib.metrics.accuracy(tf.cast(predicted_image, tf.int64), self.y, name='accuracy')
+
 
   def restore_session(self):
     global_step = 0
@@ -182,7 +222,6 @@ class SegNet:
 
     return global_step
 
-  
   def train(self, num_iterations=10000, learning_rate=1e-6):
     # Restore previous session if exists
     current_step = self.restore_session()
@@ -193,8 +232,8 @@ class SegNet:
     for i in range(current_step, num_iterations):
 
       # One training step
-      image, ground_truth = dataset.next_train_pair()
-      feed_dict = {self.x: [image], self.y: [ground_truth], self.rate: learning_rate}
+      images, ground_truths = dataset.next_train_batch()
+      feed_dict = {self.x: images, self.y: ground_truths, self.rate: learning_rate}
       print('run train step: '+str(i))
       self.train_step.run(session=self.session, feed_dict=feed_dict)
 
@@ -202,18 +241,6 @@ class SegNet:
       if i % 10 == 0:
         train_loss = self.session.run(self.loss, feed_dict=feed_dict)
         print("Step: %d, Train_loss:%g" % (i, train_loss))
-
-      # Run against validation dataset for 100 iterations
-      if i % 100 == 0:
-        image, ground_truth = dataset.next_val_pair()
-        feed_dict = {self.x: [image], self.y: [ground_truth], self.rate: learning_rate}
-        val_loss = self.session.run(self.loss, feed_dict=feed_dict)
-        val_accuracy = self.session.run(self.accuracy, feed_dict=feed_dict)
-        print("%s ---> Validation_loss: %g" % (datetime.datetime.now(), val_loss))
-        print("%s ---> Validation_accuracy: %g" % (datetime.datetime.now(), val_accuracy))
-
-        # Save the model variables
-        self.saver.save(self.session, self.checkpoint_directory + 'segnet', global_step = i)
 
   def test(self, learning_rate=1e-6):
     dataset = DatasetReader()

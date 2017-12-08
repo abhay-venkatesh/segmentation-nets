683b1e80174fed1e8ead01b4e5cc4566c00080f4
abhay-venkatesh <abhay.venkatesh@gmail.com>
2017-12-08 16:52:05 -0600

    Initialize weights from VGG

--- lib/SegNet.py
+++ lib/SegNet.py
@@ -15,7 +15,23 @@ class SegNet:
   and https://arxiv.org/pdf/1505.07293.pdf
   and https://arxiv.org/pdf/1511.00561.pdf '''
 
+  def load_vgg_weights(self):
+    vgg_path = "models/imagenet-vgg-verydeep-19.mat"
+    vgg_mat = scipy.io.loadmat(vgg_path)
+    self.vgg_params = np.squeeze(vgg_mat['layers'])
+    self.layers = ('conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
+            'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
+            'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
+            'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
+            'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
+            'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
+            'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
+            'relu5_3', 'conv5_4', 'relu5_4')
+
   def __init__(self):
+    # Load VGG model weights to initialize network weights to
+    self.load_vgg_weights()
+
     # Build the network
     self.build()
 
@@ -26,6 +42,17 @@ class SegNet:
     # Make saving trained weights and biases possible
     self.saver = tf.train.Saver(max_to_keep = 5, keep_checkpoint_every_n_hours = 1)
     self.checkpoint_directory = './checkpoints/'
+
+  def vgg_weight_and_bias(self, name, W_shape, b_shape):
+    if name not in self.layers:
+      raise KeyError("Layer missing in VGG model or mispelled. ")
+    else:
+      w, b = self.vgg_params[self.layers.index(name)][0][0][0][0]
+      init_w = tf.constant(value=np.transpose(w, (1, 0, 2, 3)), dtype=tf.float32, shape=W_shape)
+      init_b = tf.constant(value=b.reshape(-1), dtype=tf.float32, shape=b_shape)
+      w_var = tf.Variable(init_w)
+      b_var = tf.Variable(init_b)
+      return w_var, b_var 
 
   def weight_variable(self, shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -92,8 +119,9 @@ class SegNet:
     return tf.expand_dims(tf.sparse_tensor_to_dense(tf.sparse_reorder(delta)), 0)
         
   def conv_layer(self, x, W_shape, b_shape, name, padding='SAME'):
-    W = self.weight_variable(W_shape)
-    b = self.bias_variable([b_shape])
+    # Pass b_shape as list because need the object to be iterable for the constant initializer
+    W, b = self.vgg_weight_and_bias(name, W_shape, [b_shape])
+
     output = tf.nn.conv2d(x, W, strides=[1,1,1,1], padding=padding) + b
     return tf.nn.relu(output)
 
@@ -112,60 +140,60 @@ class SegNet:
     self.rate = tf.placeholder(tf.float32, shape=[])
 
     # First encoder
-    conv_1_1 = self.conv_layer(self.x, [3, 3, 3, 64], 64, 'conv_1_1')
-    conv_1_2 = self.conv_layer(conv_1_1, [3, 3, 64, 64], 64, 'conv_1_2')
+    conv_1_1 = self.conv_layer(self.x, [3, 3, 3, 64], 64, 'conv1_1')
+    conv_1_2 = self.conv_layer(conv_1_1, [3, 3, 64, 64], 64, 'conv1_2')
     pool_1, pool_1_argmax = self.pool_layer(conv_1_2)
 
     # Second encoder
-    conv_2_1 = self.conv_layer(pool_1, [3, 3, 64, 128], 128, 'conv_2_1')
-    conv_2_2 = self.conv_layer(conv_2_1, [3, 3, 128, 128], 128, 'conv_2_2')
+    conv_2_1 = self.conv_layer(pool_1, [3, 3, 64, 128], 128, 'conv2_1')
+    conv_2_2 = self.conv_layer(conv_2_1, [3, 3, 128, 128], 128, 'conv2_2')
     pool_2, pool_2_argmax = self.pool_layer(conv_2_2)
 
     # Third encoder
-    conv_3_1 = self.conv_layer(pool_2, [3, 3, 128, 256], 256, 'conv_3_1')
-    conv_3_2 = self.conv_layer(conv_3_1, [3, 3, 256, 256], 256, 'conv_3_2')
-    conv_3_3 = self.conv_layer(conv_3_2, [3, 3, 256, 256], 256, 'conv_3_3')
+    conv_3_1 = self.conv_layer(pool_2, [3, 3, 128, 256], 256, 'conv3_1')
+    conv_3_2 = self.conv_layer(conv_3_1, [3, 3, 256, 256], 256, 'conv3_2')
+    conv_3_3 = self.conv_layer(conv_3_2, [3, 3, 256, 256], 256, 'conv3_3')
     pool_3, pool_3_argmax = self.pool_layer(conv_3_3)
 
     # Fourth encoder
-    conv_4_1 = self.conv_layer(pool_3, [3, 3, 256, 512], 512, 'conv_4_1')
-    conv_4_2 = self.conv_layer(conv_4_1, [3, 3, 512, 512], 512, 'conv_4_2')
-    conv_4_3 = self.conv_layer(conv_4_2, [3, 3, 512, 512], 512, 'conv_4_3')
+    conv_4_1 = self.conv_layer(pool_3, [3, 3, 256, 512], 512, 'conv4_1')
+    conv_4_2 = self.conv_layer(conv_4_1, [3, 3, 512, 512], 512, 'conv4_2')
+    conv_4_3 = self.conv_layer(conv_4_2, [3, 3, 512, 512], 512, 'conv4_3')
     pool_4, pool_4_argmax = self.pool_layer(conv_4_3)
 
     # Fifth encoder
-    conv_5_1 = self.conv_layer(pool_4, [3, 3, 512, 512], 512, 'conv_5_1')
-    conv_5_2 = self.conv_layer(conv_5_1, [3, 3, 512, 512], 512, 'conv_5_2')
-    conv_5_3 = self.conv_layer(conv_5_2, [3, 3, 512, 512], 512, 'conv_5_3')
+    conv_5_1 = self.conv_layer(pool_4, [3, 3, 512, 512], 512, 'conv5_1')
+    conv_5_2 = self.conv_layer(conv_5_1, [3, 3, 512, 512], 512, 'conv5_2')
+    conv_5_3 = self.conv_layer(conv_5_2, [3, 3, 512, 512], 512, 'conv5_3')
     pool_5, pool_5_argmax = self.pool_layer(conv_5_3)
 
     # First decoder
     unpool_5 = self.unpool_layer2x2(pool_5, pool_5_argmax, tf.shape(conv_5_3))
-    deconv_5_3 = self.deconv_layer(unpool_5, [3, 3, 512, 512], 512, 'deconv_5_3')
-    deconv_5_2 = self.deconv_layer(deconv_5_3, [3, 3, 512, 512], 512, 'deconv_5_2')
-    deconv_5_1 = self.deconv_layer(deconv_5_2, [3, 3, 512, 512], 512, 'deconv_5_1')
+    deconv_5_3 = self.deconv_layer(unpool_5, [3, 3, 512, 512], 512, 'deconv5_3')
+    deconv_5_2 = self.deconv_layer(deconv_5_3, [3, 3, 512, 512], 512, 'deconv5_2')
+    deconv_5_1 = self.deconv_layer(deconv_5_2, [3, 3, 512, 512], 512, 'deconv5_1')
 
     # Second decoder
     unpool_4 = self.unpool_layer2x2(deconv_5_1, pool_4_argmax, tf.shape(conv_4_3))
-    deconv_4_3 = self.deconv_layer(unpool_4, [3, 3, 512, 512], 512, 'deconv_4_3')
-    deconv_4_2 = self.deconv_layer(deconv_4_3, [3, 3, 512, 512], 512, 'deconv_4_2')
-    deconv_4_1 = self.deconv_layer(deconv_4_2, [3, 3, 256, 512], 256, 'deconv_4_1')
+    deconv_4_3 = self.deconv_layer(unpool_4, [3, 3, 512, 512], 512, 'deconv4_3')
+    deconv_4_2 = self.deconv_layer(deconv_4_3, [3, 3, 512, 512], 512, 'deconv4_2')
+    deconv_4_1 = self.deconv_layer(deconv_4_2, [3, 3, 256, 512], 256, 'deconv4_1')
 
     # Third decoder
     unpool_3 = self.unpool_layer2x2(deconv_4_1, pool_3_argmax, tf.shape(conv_3_3))
-    deconv_3_3 = self.deconv_layer(unpool_3, [3, 3, 256, 256], 256, 'deconv_3_3')
-    deconv_3_2 = self.deconv_layer(deconv_3_3, [3, 3, 256, 256], 256, 'deconv_3_2')
-    deconv_3_1 = self.deconv_layer(deconv_3_2, [3, 3, 128, 256], 128, 'deconv_3_1')
+    deconv_3_3 = self.deconv_layer(unpool_3, [3, 3, 256, 256], 256, 'deconv3_3')
+    deconv_3_2 = self.deconv_layer(deconv_3_3, [3, 3, 256, 256], 256, 'deconv3_2')
+    deconv_3_1 = self.deconv_layer(deconv_3_2, [3, 3, 128, 256], 128, 'deconv3_1')
 
     # Fourth decoder
     unpool_2 = self.unpool_layer2x2(deconv_3_1, pool_2_argmax, tf.shape(conv_2_2))
-    deconv_2_2 = self.deconv_layer(unpool_2, [3, 3, 128, 128], 128, 'deconv_2_2')
-    deconv_2_1 = self.deconv_layer(deconv_2_2, [3, 3, 64, 128], 64, 'deconv_2_1')
+    deconv_2_2 = self.deconv_layer(unpool_2, [3, 3, 128, 128], 128, 'deconv2_2')
+    deconv_2_1 = self.deconv_layer(deconv_2_2, [3, 3, 64, 128], 64, 'deconv2_1')
 
     # Fifth decoder
     unpool_1 = self.unpool_layer2x2(deconv_2_1, pool_1_argmax, tf.shape(conv_1_2))
-    deconv_1_2 = self.deconv_layer(unpool_1, [3, 3, 64, 64], 64, 'deconv_1_2')
-    deconv_1_1 = self.deconv_layer(deconv_1_2, [3, 3, 32, 64], 32, 'deconv_1_1')
+    deconv_1_2 = self.deconv_layer(unpool_1, [3, 3, 64, 64], 64, 'deconv1_2')
+    deconv_1_1 = self.deconv_layer(deconv_1_2, [3, 3, 32, 64], 32, 'deconv1_1')
 
     # Produce class scores
     preds = self.deconv_layer(deconv_1_1, [1, 1, 28, 32], 28, 'preds')

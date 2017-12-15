41df8bd770346ba4a24d884354ab050a17c42914
abhay-venkatesh <abhay.venkatesh@gmail.com>
2017-12-15 12:42:02 -0600

    Output at 75000

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from lib.BatchSegNet import BatchSegNet
 def main():
   dataset_directory = './datasets/unreal_randomyaw/'
   net = BatchSegNet(dataset_directory)
-  # net.train(num_iterations=50000, learning_rate=0.1)
-  net.test()
+  net.train(num_iterations=80000, learning_rate=1e-2)
+  # net.test()
 
 if __name__ == "__main__":
   main()

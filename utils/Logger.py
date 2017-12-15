a3bd385bcee64e80634db4faf6ac4d26679310d8
abhay-venkatesh <abhay.venkatesh@gmail.com>
2017-12-15 12:57:53 -0600

    Better testing and logging framework

--- utils/Logger.py
+++ utils/Logger.py
@@ -15,5 +15,15 @@ class Logger:
       with open('./logs/logfile-' + str(self.session), 'a') as outfile:
         outfile.write(message)
 
+  def log_for_graphing(self, iterations, loss, accuracy):
+    if not os.path.exists('./logs/logfile-graphing-' + str(self.session)):
+      with open('./logs/logfile-graping-' + str(self.session), 'w') as outfile:
+        message = str(iterations) + ',' + str(loss) + ',' + str(accuracy) + '\n'
+        outfile.write(message)
+    else:
+      with open('./logs/logfile-graping-' + str(self.session), 'a') as outfile:
+        outfile.write(message)
+
+
 
   

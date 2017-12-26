6f9aa4c575d217107a6ef6d12ccf9b6201d0a990
abhay-venkatesh <abhay.venkatesh@gmail.com>
2017-12-26 00:39:10 -0600

    Add

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from lib.BatchSegNet import BatchSegNet
 def main():
   dataset_directory = './datasets/unreal_randomyaw/'
   net = BatchSegNet(dataset_directory)
-  net.train(num_iterations=90000, learning_rate=1e-2)
+  net.train(num_iterations=100000, learning_rate=1e-2)
   # net.test()
 
 if __name__ == "__main__":

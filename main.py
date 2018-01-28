5211fdca2bfcf84c7b6deabfb0936dd65763749c
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-01-27 19:42:04 -0600

    Fix indentation issue with mini-encoder-decoder code block

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
   dataset_directory = './datasets/UnrealNeighborhood-11Class-StreetPrimary-0.15/'
   net = DFSegNet(dataset_directory)
   # net = BatchSegNet(dataset_directory)
-  net.train(num_iterations=100000, learning_rate=1e-2)
+  net.train(num_iterations=80000, learning_rate=1e-2)
 
 if __name__ == "__main__":
   main()

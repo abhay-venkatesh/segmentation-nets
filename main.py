59cca295e5778251458327ad45f6fea6a1997cca
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-02-08 14:44:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from lib.DFSegNet import DFSegNet
+from lib.BatchSegNet import BatchSegNet
 
 def main():
   dataset_directory = './datasets/UnrealNeighborhood-11Class-StreetPrimary-0.15/'
-  net = DFSegNet(dataset_directory)
+  net = BatchSegNet(dataset_directory)
   # net.train(num_iterations=80100, learning_rate=1e-2)
   net.test_sequence()
 
